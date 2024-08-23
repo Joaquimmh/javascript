2b031fd71a034578d36e255e4ea6dca9690e65d6
Joaquimmh <matheusjoaquim15@gmail.com>
2024-08-23 13:53:14 -0300

    Initial commit

---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+console.log(`Viviendo em ${País}`)
+if (país == 'Brasil') {
+    console.log('Você á Brasileiro!')
+}  else {
+    console.log('Você é Estrangeiro!')
+}
---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,22 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var hora = 17
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        // BOM DIA!
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#db8983'
+    } else if (hora >= 12 && hora < 18) {
+        // BOA TARDE!
+        img.src = 'fototarde.png'
+        document.body.style.background = '#974525'
+    } else {
+        // BOA NOITE!
+        img.scr = 'fotonoite.png'
+        document.body.style.background = '#0b181e'
+    }
+        
+}
